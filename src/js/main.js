c88d459493dd8d9eb3bec6d3791a3b5ee7ce23d4
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-05-26 18:31:18 +0200

    added project and details

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,45 @@
-window.addEventListener('resize', () => {
-  checkWindowWidth();
-  hamburgerNav.style.transition = 'none'
-});
+const details = document.querySelectorAll('.project-detail');
+const projectTitles = document.querySelectorAll('.home-projects__list-item')
 
-const navButtons = document.querySelectorAll('hamburger__nav-list a');
-const discoverButton = document.querySelector('.landing-text a');
-const hamburgerIcon = document.querySelector('.fas.fa-bars');
-const hamburgerClose = document.querySelector('.fas.fa-times');
-const hamburgerNav = document.querySelector('.hamburger__nav');
-const mainNav = document.querySelector('.main__navigation');
-
-hamburgerNav.style.left = '-100vw';
-
-hamburgerIcon.addEventListener('click', () => {
-  hamburgerNav.style.left = '0';
-  hamburgerNav.style.transition = 'all 0.5s';
+projectTitles.forEach(item => {
+  item.addEventListener('click', () => {
+    item.children[2].classList.toggle('open');
+  })
 })
 
-hamburgerClose.addEventListener('click', () => {
-  hamburgerNav.style.left = '-100vw';
-  hamburgerNav.style.transition = 'all 0.5s';
-})
+// window.addEventListener('resize', () => {
+//   checkWindowWidth();
+//   // hamburgerNav.style.transition = 'none'
+// });
 
-const checkWindowWidth = () => {
-  if (window.innerWidth <= 1024) {
-    hamburgerIcon.style.display = 'block';
-    hamburgerNav.style.display = 'absolute';
-    mainNav.style.display = 'none';
-  } else {
-    hamburgerIcon.style.display = 'none';
-    mainNav.style.display = 'block';
-  }
-}
+// const navButtons = document.querySelectorAll('hamburger__nav-list a');
+// const discoverButton = document.querySelector('.landing-text a');
+// const hamburgerIcon = document.querySelector('.fas.fa-bars');
+// const hamburgerClose = document.querySelector('.fas.fa-times');
+// const hamburgerNav = document.querySelector('.hamburger__nav');
+// const mainNav = document.querySelector('.main__navigation');
 
-checkWindowWidth();
+// // hamburgerNav.style.left = '-100vw';
+
+// // hamburgerIcon.addEventListener('click', () => {
+// //   hamburgerNav.style.left = '0';
+// //   hamburgerNav.style.transition = 'all 0.5s';
+// // })
+
+// // hamburgerClose.addEventListener('click', () => {
+// //   hamburgerNav.style.left = '-100vw';
+// //   hamburgerNav.style.transition = 'all 0.5s';
+// // })
+
+// const checkWindowWidth = () => {
+//   if (window.innerWidth <= 1024) {
+//     hamburgerIcon.style.display = 'block';
+//     // hamburgerNav.style.display = 'absolute';
+//     mainNav.style.display = 'none';
+//   } else {
+//     hamburgerIcon.style.display = 'none';
+//     mainNav.style.display = 'block';
+//   }
+// }
+
+// checkWindowWidth();
