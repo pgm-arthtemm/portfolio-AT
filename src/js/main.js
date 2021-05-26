ddffff36d2987e5bdd4ce1f9f0fa9160ea42558e
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-05-26 19:12:48 +0200

    added javascript for hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,39 +7,39 @@ projectTitles.forEach(item => {
   })
 })
 
-// window.addEventListener('resize', () => {
-//   checkWindowWidth();
-//   // hamburgerNav.style.transition = 'none'
-// });
+window.addEventListener('resize', () => {
+  checkWindowWidth();
+  hamburgerNav.style.transition = 'none'
+});
 
-// const navButtons = document.querySelectorAll('hamburger__nav-list a');
-// const discoverButton = document.querySelector('.landing-text a');
-// const hamburgerIcon = document.querySelector('.fas.fa-bars');
-// const hamburgerClose = document.querySelector('.fas.fa-times');
-// const hamburgerNav = document.querySelector('.hamburger__nav');
-// const mainNav = document.querySelector('.main__navigation');
+const navButtons = document.querySelectorAll('hamburger__nav-list a');
+const discoverButton = document.querySelector('.landing-text a');
+const hamburgerIcon = document.querySelector('.fas.fa-bars');
+const hamburgerClose = document.querySelector('.fas.fa-times');
+const hamburgerNav = document.querySelector('.hamburger__nav');
+const mainNav = document.querySelector('.main__navigation');
 
-// // hamburgerNav.style.left = '-100vw';
+hamburgerNav.style.left = '-100vw';
 
-// // hamburgerIcon.addEventListener('click', () => {
-// //   hamburgerNav.style.left = '0';
-// //   hamburgerNav.style.transition = 'all 0.5s';
-// // })
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerNav.style.left = '0';
+  hamburgerNav.style.transition = 'all 0.5s';
+})
 
-// // hamburgerClose.addEventListener('click', () => {
-// //   hamburgerNav.style.left = '-100vw';
-// //   hamburgerNav.style.transition = 'all 0.5s';
-// // })
+hamburgerClose.addEventListener('click', () => {
+  hamburgerNav.style.left = '-100vw';
+  hamburgerNav.style.transition = 'all 0.5s';
+})
 
-// const checkWindowWidth = () => {
-//   if (window.innerWidth <= 1024) {
-//     hamburgerIcon.style.display = 'block';
-//     // hamburgerNav.style.display = 'absolute';
-//     mainNav.style.display = 'none';
-//   } else {
-//     hamburgerIcon.style.display = 'none';
-//     mainNav.style.display = 'block';
-//   }
-// }
+const checkWindowWidth = () => {
+  if (window.innerWidth <= 1024) {
+    hamburgerIcon.style.display = 'block';
+    hamburgerNav.style.display = 'absolute';
+    mainNav.style.display = 'none';
+  } else {
+    hamburgerIcon.style.display = 'none';
+    mainNav.style.display = 'block';
+  }
+}
 
-// checkWindowWidth();
+checkWindowWidth();
