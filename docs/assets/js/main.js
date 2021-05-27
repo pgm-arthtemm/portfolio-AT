cd484824475d127f01c2ec12fb8dd1e149c21cee
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-05-27 20:11:23 +0200

    made projects responsive

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -16,6 +16,7 @@ const hamburgerIcon = document.querySelector('.fas.fa-bars');
 const hamburgerClose = document.querySelector('.fas.fa-times');
 const hamburgerNav = document.querySelector('.hamburger__nav');
 const mainNav = document.querySelector('.main__navigation');
+const mobileLogo = document.querySelector('.hamburger__menu-heading a');
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerNav.style.display = 'block';
@@ -23,7 +24,6 @@ hamburgerIcon.addEventListener('click', () => {
 
 hamburgerClose.addEventListener('click', () => {
   hamburgerNav.style.display = 'none';
-  console.log('closed')
 })
 
 const checkWindowWidth = () => {
@@ -31,9 +31,11 @@ const checkWindowWidth = () => {
     hamburgerIcon.style.display = 'block';
     hamburgerNav.style.display = 'absolute';
     mainNav.style.display = 'none';
+    mobileLogo.style.display = 'block';
   } else {
     hamburgerIcon.style.display = 'none';
     mainNav.style.display = 'block';
+    mobileLogo.style.display = 'none';
   }
 }
 
