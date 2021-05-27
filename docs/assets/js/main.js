126d814d2b3879ba00ef236a0c27c9b47d3e7fdf
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-05-27 18:48:53 +0200

    full screen hamburger menu for mobile

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -12,23 +12,13 @@ window.addEventListener('resize', () => {
   hamburgerNav.style.transition = 'none'
 });
 
-const navButtons = document.querySelectorAll('hamburger__nav-list a');
-const discoverButton = document.querySelector('.landing-text a');
 const hamburgerIcon = document.querySelector('.fas.fa-bars');
 const hamburgerClose = document.querySelector('.fas.fa-times');
 const hamburgerNav = document.querySelector('.hamburger__nav');
 const mainNav = document.querySelector('.main__navigation');
 
-hamburgerNav.style.left = '-100vw';
-
 hamburgerIcon.addEventListener('click', () => {
-  hamburgerNav.style.left = '0';
-  hamburgerNav.style.transition = 'all 0.5s';
-})
-
-hamburgerClose.addEventListener('click', () => {
-  hamburgerNav.style.left = '-100vw';
-  hamburgerNav.style.transition = 'all 0.5s';
+  console.log('fukc')
 })
 
 const checkWindowWidth = () => {
